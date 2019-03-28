844cf729302826d72905f9f831069a3f5ae1deb8
qili12 <1358178391@qq.com>
2019-03-28 13:20:50 +0800

    first

---
+++ 四则运算/四则运算Tests/ProgramTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 四则运算;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace 四则运算.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            int i = 0;
+            if (Program.arrayList.Contains('.'))
+                i = 1;
+            Assert.IsTrue(i == 0);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChuTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
